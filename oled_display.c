108247ddbc7467357731bef9329942e372a0c08f
Luc <8822552+luc-github@users.noreply.github.com>
2025-02-25 21:04:44 +0800

    Update oled_display.c

--- oled_display.c
+++ oled_display.c
@@ -24,18 +24,30 @@
 #include "../driver.h"
 #include "../grbl/hal.h"
 #include "../grbl/task.h"
+#include "../grbl/system.h"
 #else
 #include "driver.h"
 #include "grbl/hal.h"
 #include "grbl/task.h"
+#include "grbl/system.h"
 #endif //ARDUINO
-
 #include "grbl/report.h"
+
+#define POLLING_DELAY 800
 
 #if defined(OLED_DISPLAY_ENABLE)
 #include "oled_display.h"
 static on_report_options_ptr on_report_options;
 static on_state_change_ptr on_state_change;
+
+typedef struct {
+  const char *state;
+  char ip[16];
+  double pos[N_AXIS];
+  bool end_stop[N_AXIS];
+} oled_screen_t;
+
+static oled_screen_t screen1;
 
 #if ETHERNET_ENABLE || WIFI_ENABLE
 #ifdef ARDUINO
@@ -51,6 +63,7 @@ static void network_event (const char *interface, network_status_t status)
     if(on_event){
       on_event(interface, status);
     }
+    
     report_info("network event");
 }
 
@@ -73,16 +86,53 @@ static void onStateChanged (sys_state_t state)
     if(on_state_change){
       on_state_change(state);
     }
-    report_info("State changed");
+    switch(state) {
+      case STATE_IDLE:
+        screen1.state = "Idle";
+        break;
+      case STATE_CHECK_MODE:
+        screen1.state = "Check";
+        break;
+      case STATE_HOMING:
+        screen1.state = "Home";
+        break;
+      case STATE_JOG:
+        screen1.state = "Jog";
+        break;
+      case STATE_CYCLE:
+        screen1.state = "Run";
+        break;
+      case STATE_HOLD:
+        screen1.state = "Hold";
+        break;
+      case STATE_SAFETY_DOOR:
+        screen1.state = "Door";
+        break;
+      case STATE_SLEEP:
+        screen1.state = "Sleep";
+        break;
+      case STATE_ESTOP:
+      case STATE_ALARM:
+        screen1.state = "Alarm";
+        break;
+      case STATE_TOOL_CHANGE:
+      default:
+        break;
+    };
 }
 
-static void polling_fn (void *data){
-  report_info("polling_fn");
-  task_add_delayed(polling_fn, NULL, 500);
+static void polling_task (void *data){
+  task_add_delayed(polling_task, NULL, POLLING_DELAY);
+  report_info((void *)screen1.state);
+  report_info((void *)screen1.ip);
 }
 
 void oled_display_init (void)
 {
+    screen1.state = "IDLE";
+    strcpy(screen1.ip, "0.0.0.0");
+    memset(screen1.pos, 0, sizeof(screen1.pos));
+    memset(screen1.end_stop, 0, sizeof(screen1.end_stop));
     // hook report options
     on_report_options = grbl.on_report_options;
     grbl.on_report_options = report_options;
@@ -96,8 +146,9 @@ void oled_display_init (void)
     on_event = networking.event;
     networking.event = network_event;
     #endif
-    // add polling task
-    task_add_delayed(polling_fn, NULL, 500);
+
+    // start polling task
+    task_add_delayed(polling_task, NULL, POLLING_DELAY);
 }
 
 #endif //OLED_DISPLAY_ENABLE
